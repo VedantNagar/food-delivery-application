5e75e9f25ca4515a1b4de1b9fd8f13227f5970ef
Kshitij <kshitij.tanwar2002@gmail.com>
2023-12-13 21:29:49 +0530

    responsive navbar

--- frontend/src/Homepage/Hero-section/Navbar.jsx
+++ frontend/src/Homepage/Hero-section/Navbar.jsx
@@ -1,30 +1,46 @@
 import classes from "./Navbar.module.css";
 import logo from "../../images/logo.svg";
-
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
     return (
-        <div className={classes.nav}>
-            <div className={classes.left}>
-                <img src={logo} alt="not found" className={classes.img} />
-                <h2>Fudo</h2>
-            </div>
-            <div className={classes.right}>
-                <ul>
-                    <li>
-                        <a href="#">Sign in</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Menu</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <header>
+            <nav>
+                <div className={classes.left}>
+                    <div className={classes.logo}>
+                        <img
+                            src={logo}
+                            alt="not found"
+                            className={classes.img}
+                        />
+                        <h2>Fudo</h2>
+                    </div>
+                    <button className={classes.navBtn} onClick={toggleMenu}>
+                        <MenuIcon />
+                    </button>
+                </div>
+                <div className={classes.right}>
+                    <ul className={isMenuOpen ? `${classes.right}`: `${classes.right} ${classes.hide}`}>
+                        <li>
+                            <a href="#">Sign in</a>
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                        </li>
+                        <li>
+                            <a href="#">Menu</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     );
 };
 export default Navbar;
