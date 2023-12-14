d1416c79ef9568bc52bce6c891652bd2365cac99
Kshitij <kshitij.tanwar2002@gmail.com>
2023-12-14 19:05:37 +0530

    folder arrangement

---
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Hero-section/Navbar";
+import Signin from "./Hero-section/Signin";
+
+const Homepage = () => {
+    return (
+        <>
+            <Navbar />
+            <Signin />
+        </>
+    );
+};
+export default Homepage;
