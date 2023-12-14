40ef5751ad3e5e4b61bb70a9f648033f3397ab5d
Kshitij <kshitij.tanwar2002@gmail.com>
2023-12-14 20:32:37 +0530

    responsive services

--- frontend/src/Components/Homepage/Homepage.jsx
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./Hero-section/Navbar";
 import Signin from "./Hero-section/Signin";
+import Services from "../Services/Services";
 
 const Homepage = () => {
     return (
         <>
             <Navbar />
             <Signin />
+            <Services />
         </>
     );
 };
---
+++ frontend/src/Components/Services/Services.jsx
@@ -0,0 +1,42 @@
+import classes from "./Services.module.css";
+import service1 from "../../images/service-1.svg";
+import service2 from "../../images/service-2.svg";
+import service3 from "../../images/service-3.svg";
+const Services = () => {
+    return (
+        <div className={classes.container}>
+            <h3 className={classes.header}>What we serve</h3>
+            <h1>
+                Your Favourite Food <br />
+                Delivery Partner
+            </h1>
+            <div className={classes.cards}>
+                <div className={classes.card}>
+                    <img src={service1} alt="not found" />
+                    <h3>Easy to Order</h3>
+                    <p>
+                        You only need a few steps in <br />
+                        ordering food
+                    </p>
+                </div>
+                <div className={classes.card}>
+                    <img src={service2} alt="not found" />
+                    <h3>Fastest Delivery</h3>
+                    <p>
+                        Delivery that is always ontime
+                        <br /> even faster
+                    </p>
+                </div>
+                <div className={classes.card}>
+                    <img src={service3} alt="not found" />
+                    <h3>Best Quality</h3>
+                    <p>
+                        Not only fast for us quality is also <br />
+                        number one
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default Services;
