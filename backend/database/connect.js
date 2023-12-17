f4365bb073df67213e61223bb8ea098043224352
Vedant <vedantnagar21@gmail.com>
2023-12-17 13:38:54 +0530

    Updated Controllers

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = (url) => {
     return mongoose.connect(url,{
-        useNewUrlParser: true,
+        //useNewUrlParser: true,
         // useCreateIndex: true,
         // useFindAndModify: false,
         // useUnifiedTopology: true,
