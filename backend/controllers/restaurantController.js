4ced4a7bcc6c2ea3ea103b3d892806986bcbc639
Vedant <vedantnagar21@gmail.com>
2023-12-17 13:34:35 +0530

    Updated Controllers

--- backend/controllers/restaurantController.js
+++ backend/controllers/restaurantController.js
@@ -5,24 +5,70 @@ const restaurantModel = require('../models/restaurant');
 
 //get rest(id) 
 const getRestaurant = async (req,res) => {
-    const { id: restaurantID } = req.params
-  const restaurant = await restaurantModel.findOne({ _id: restaurantID })
+    const { id: restaurantID } = req.params;
+    const restaurant = await restaurantModel.findOne({_id: restaurantID})
   if (!restaurant) {
-    res.status(404).json("not find")
+    res.status(404).json("Restaurant not found")
   }
-
   res.status(200).json({ restaurant })
 }
+/*
+const getRestaurant = async (req, res) => {
+    const {id: restaurantID} = req.params;
+    try {
+        const restaurant = await restaurantModel.findById(restaurantID);
+        if (!restaurant) {
+            return res.status(404).json("Restaurant not found");
+        }
+        res.status(200).json({restaurant});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error:Internal Server Error});
+    }
+};*/ 
 
 //get all rest
 const getAllRestaurant = async (req,res) => {
+    try{
     const restaurants = await restaurantModel.find({})
     res.status(200).json({restaurants})
+}catch(error){
+    console.log(error);
+    res.status(500).json({error:'Internal server error'});
 }
+};
 
 //delete rest(id)
 
+/*const deleteRestaurant = async (req, res) => {
+    const {id: restaurantID} = req.params;
+    try{
+        const deletedRestaurant = await restaurantModel.findOneAndDelete({ _id:restaurantID});
+        if (!deletedRestaurant){
+            return res.status(404).json("Restaurant not found");
+        }
+        res.status(200).json({message:"Restaurant deleted successfully"});
+    }catch (error){
+        console.error(error);
+        res.status(500).json({error:'Internal server error'});
+    }
+};*/
+
 //edit rest(id) -> menu,add image,description
+/*const editRestaurant = async(req, res)=>{
+    const {id:restaurantID}=req.params;
+    try {
+        const updatedRestaurant= await restaurantModel.findByIdAndUpdate(restaurantID, req.body,{new:true});
+
+        if(!updatedRestaurant){
+            return res.status(404).json('Restaurant not found');
+        }
+        res.status(200).json({updatedRestaurant});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error:'Internal server error'});
+    }
+};*/
 
 //create
 const createRestaurant = async (req, res) => {
@@ -41,4 +87,4 @@ const createRestaurant = async (req, res) => {
 
 // post menu
 
-module.exports = { createRestaurant ,getRestaurant,getAllRestaurant};
+module.exports = { createRestaurant ,getRestaurant,getAllRestaurant,deleteRestaurant,editRestaurant};
