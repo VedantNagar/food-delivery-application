ea7f657664f7ecbbe1b4c17a4e81e40db90e1cb9
Kshitij <kshitij.tanwar2002@gmail.com>
2023-12-20 00:52:03 +0530

    array obj in nav

--- frontend/src/Components/Homepage/Homepage.jsx
+++ frontend/src/Components/Homepage/Homepage.jsx
@@ -4,17 +4,23 @@ import Signin from "./Hero-section/Signin";
 import Menu from "./Menu/Menu";
 import Services from "./Services/Services";
 import classes from "./Homepage.module.css";
+const navbarMain = [
+    { title: "Sign In" },
+    { title: "Services" },
+    { title: "Menu" },
+    { title: "Contact" },
+];
 const Homepage = () => {
     return (
         <>
             <div className={classes.wrapper}>
-                <Navbar />
+                <Navbar list={navbarMain} />
                 <Signin />
                 <Services />
                 <Menu />
             </div>
             <div className={classes.footer}>
-            <Footer />
+                <Footer />
             </div>
         </>
     );
