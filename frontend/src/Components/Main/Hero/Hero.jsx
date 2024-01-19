6b9fb584101ab431ee3b287bd97910e6cbbff6a8
Kshitij <kshitij.tanwar2002@gmail.com>
2024-01-20 01:38:50 +0530

    Hero

--- frontend/src/Components/Main/Hero/Hero.jsx
+++ frontend/src/Components/Main/Hero/Hero.jsx
@@ -1,9 +1,11 @@
+import SearchBar from "../../Utils/SearchBar/SearchBar"
 import hero from "../images/hero.svg"
 import classes from "./Hero.module.css"
 const Hero = () => {
   return (
     <div className={classes.hero}>
         <img src={hero} alt="" />
+        <SearchBar/>
     </div>
   )
 }
---
+++ frontend/src/Components/Utils/SearchBar/SearchBar.jsx
@@ -0,0 +1,38 @@
+import classes from "./SearchBar.module.css";
+import location from "../../Main/images/location.svg";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import search from "../../Main/images/Search.svg";
+const area = [
+    { label: "Janakpuri" },
+    { label: "Patel Nagar" },
+    { label: "Shalimar Bagh" },
+    { label: "Rajendra Nagar" },
+    { label: "Paschim Vihar" },
+];
+const SearchBar = () => {
+    return (
+        <div className={classes.searchBar}>
+            <div className={classes.left}>
+                <img src={location} alt="" />
+                <Autocomplete
+                    sx={{ width: 200 }}
+                    options={area}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Select a location"
+                        ></TextField>
+                    )}
+                />
+            </div>
+            <div className={classes.right}>
+                <img src={search} />
+                <form action="#">
+                    <input type="text" placeholder="Search for restaurant, cuisine or a dish"/>
+                </form>
+            </div>
+        </div>
+    );
+};
+export default SearchBar;
