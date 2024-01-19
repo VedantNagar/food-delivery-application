ba5b6748f17b982e894b25eb1ba7b388196751b3
Kshitij <kshitij.tanwar2002@gmail.com>
2024-01-20 00:00:26 +0530

    main page

--- frontend/src/Components/Main/Main.jsx
+++ frontend/src/Components/Main/Main.jsx
@@ -1,13 +1,13 @@
 import Navbar from "../Homepage/Hero-section/Navbar";
 import classes from "./Main.module.css";
 import dropdown from "./images/dropdown.svg";
-import cart from "./images/cart.svg"
-import user from "./images/user.svg"
+import cart from "./images/cart.svg";
+import user from "./images/user.svg";
 const navbarMain = [
     { title: "Order Now", imgfwd: "", imgbwd: "" },
-    { title: "Top Brands", imgfwd: dropdown , imgbwd: ""},
-    { title: "Cart", imgfwd: cart , imgbwd: ""},
-    { title: "Kshitij", imgfwd: dropdown , imgbwd: user},
+    { title: "Top Brands", imgfwd: dropdown, imgbwd: "" },
+    { title: "Cart", imgfwd: cart, imgbwd: "" },
+    { title: "Kshitij", imgfwd: dropdown, imgbwd: user },
 ];
 const Main = () => {
     return (
