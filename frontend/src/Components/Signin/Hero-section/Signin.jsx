85d795c8750bb6beb09fe35e1559c45657751a99
Kshitij <kshitij.tanwar2002@gmail.com>
2024-02-08 19:51:35 +0530

    signin page

--- frontend/src/Components/Signin/Hero-section/Signin.jsx
+++ frontend/src/Components/Signin/Hero-section/Signin.jsx
@@ -1,10 +1,10 @@
 import classes from "./Signin.module.css";
 import heroImg from "../../../images/hero-section-landing.svg";
 import { Button } from "@mui/material";
-import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
-import GoogleIcon from "@mui/icons-material/Google";
-import AppleIcon from "@mui/icons-material/Apple";
+import { Link, Form, useSearchParams } from "react-router-dom";
 const Signin = () => {
+    const [searchParams] = useSearchParams();
+    const isLogin = searchParams.get("mode") === "login";
     const submitHandler = (e) => {
         e.preventDefault();
     };
@@ -13,10 +13,15 @@ const Signin = () => {
             <div className={classes.left}>
                 <h1>Welcome Back.</h1>
                 <h3>Let's sign you in.</h3>
-                <img src={heroImg} alt="not found" className={classes.signInImg}/>
+                <img
+                    src={heroImg}
+                    alt="not found"
+                    className={classes.signInImg}
+                />
             </div>
             <div className={classes.right}>
-                <form action="#" className={classes.formAction}>
+                <Form method="post" className={classes.formAction}>
+                    <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
                     <input
                         type="email"
                         name="email"
@@ -29,59 +34,38 @@ const Signin = () => {
                         id="password"
                         placeholder="Password"
                     />
-                </form>
-                <Button
-                    variant="contained"
-                    style={{
-                        backgroundColor: "#FF4040",
-                        fontSize: "1.175rem",
-                        textTransform: "none",
-                        padding: "13px 110px",
-                        
-                    }}
-                    sx={{
-                        width:{
-                            xs:"17rem",
-                            sm:"17rem",
-                            md: "19rem",
-                            lg:"19rem",
-                            xl:"19rem"
-                        }
-                    }}
-                    disableElevation
-                    onClick={submitHandler}
-                >
-                    Continue
-                </Button>
-                <div className={classes.icons}>
-                    <FacebookTwoToneIcon
+
+                    <Button
+                        variant="contained"
                         style={{
-                            width: "3rem",
-                            height: "3rem",
-                            border: "1px solid black",
-                            borderRadius: "100%",
+                            backgroundColor: "#FF4040",
+                            fontSize: "1.175rem",
+                            textTransform: "none",
+                            padding: "13px 110px",
                         }}
-                    />
-                    <GoogleIcon
-                        style={{
-                            width: "3rem",
-                            height: "3rem",
-                            border: "1px solid black",
-                            borderRadius: "100%",
+                        sx={{
+                            width: {
+                                xs: "17rem",
+                                sm: "17rem",
+                                md: "19rem",
+                                lg: "19rem",
+                                xl: "19rem",
+                            },
                         }}
-                    />
-                    <AppleIcon
-                        style={{
-                            width: "3rem",
-                            height: "3rem",
-                            border: "1px solid black",
-                            borderRadius: "100%",
-                        }}
-                    />
-                </div>
+                        disableElevation
+                        onClick={submitHandler}
+                    >
+                        Continue
+                    </Button>
                     <p>
-                        Don't have an account? <a href="#">Sign up</a>
+                        Want to
+                        <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
+                            {isLogin ? " Create new user " : " Login "}
+                        </Link>
+                        ?
                     </p>
+                </Form>
+                
             </div>
         </div>
     );
