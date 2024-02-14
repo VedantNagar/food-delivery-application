3c39200c221ee0f6f3d6b545f3020f3fb65da306
Kshitij <kshitij.tanwar2002@gmail.com>
2024-02-14 21:39:13 +0530

    Menu card

--- frontend/src/Components/RestaurantPage/Menu/MenuCard.jsx
+++ frontend/src/Components/RestaurantPage/Menu/MenuCard.jsx
@@ -2,7 +2,7 @@ import classes from "./MenuCard.module.css";
 import foodImg from "../images/foodImg.svg";
 
 const MenuCard = ({ item }) => {
-    console.log(item)
+    console.log(item);
     const head = item?.name ?? "Brunch for 2 - Veg (Save upto Rs.45)";
     const price = item?.price ?? "599";
     const image = item?.image?.length > 0 ? item.image : foodImg;
@@ -12,16 +12,14 @@ const MenuCard = ({ item }) => {
         "Brunch: One meal to rule them all! Grab this mega saver combo with your choice of 2 veg wraps, Aloo Paratha (2 pcs), chole and Curd lunchbox and 2 choco lava cakes. This is just bliss on a plate!";
     return (
         <div className={classes.menu}>
-            <div className={classes.right}>
-                <div className={classes.info}>
-                    <h4 className={classes.head}>{head}</h4>
-                    <h4 className={classes.price}>₹{price}</h4>
-                    <p className={classes.desc}>{desc}</p>
-                </div>
-                <div className={classes.image}>
-                    <img src={image} alt="" />
-                    <a className={classes.add}>Add +</a>
-                </div>
+            <div className={classes.info}>
+                <h4 className={classes.head}>{head}</h4>
+                <h4 className={classes.price}>₹{price}</h4>
+                <p className={classes.desc}>{desc}</p>
+            </div>
+            <div className={classes.image}>
+                <img src={image} alt="" />
+                <a className={classes.add}>Add +</a>
             </div>
         </div>
     );
