ca5069125d9bdaa0819923db3c983459ced1fe17
Kshitij <kshitij.tanwar2002@gmail.com>
2024-02-16 02:41:28 +0530

    logout functionality

--- frontend/src/Components/Utils/DropdownMenu/DropdownMenu.jsx
+++ frontend/src/Components/Utils/DropdownMenu/DropdownMenu.jsx
@@ -1,41 +1,51 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { userContext } from "../../../userContext/context";
 export default function DropdownMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  return (
-    <div>
-      <Button
-        id="basic-button"
-        aria-controls={open ? 'basic-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        onClick={handleClick}
-      >
-        Dashboard
-      </Button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          'aria-labelledby': 'basic-button',
-        }}
-      >
-        <MenuItem onClick={handleClose}>Orders</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
-      </Menu>
-    </div>
-  );
+  const navigate = useNavigate();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        event.preventDefault();
+        setAnchorEl(event.currentTarget);
+    };
+    const { logOut } = useContext(userContext);
+    const logoutHandler = (e) => {
+        e.preventDefault();
+        logOut();
+        navigate("/");
+        setAnchorEl(null);
+    };
+    const handleClose = (e) => {
+        e.preventDefault();
+        setAnchorEl(null);
+    };
+    return (
+        <div>
+            <Button
+                id="basic-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+            >
+                Account
+            </Button>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    "aria-labelledby": "basic-button",
+                }}
+            >
+                <MenuItem onClick={handleClose}>Orders</MenuItem>
+                <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+            </Menu>
+        </div>
+    );
 }
