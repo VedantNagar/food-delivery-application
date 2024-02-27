ed2252853d987c08b90c7f9dcd5d50cc594f2dc1
Vedant <vedantnagar21@gmail.com>
2024-02-27 23:40:08 +0530

    blank push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,63 +1,63 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Cart from "./Components/Cart/Cart";
-import SignIn from "./Components/Signin/Signin";
-import HomePage from "./Components/Homepage/Homepage";
-import RestaurantPage from "./Components/RestaurantPage/RestaurantPage";
-import { UserContextProvider } from "./userContext/context.jsx";
-import "./index.css";
-import Dashboard from "./Components/Dashboard/Dashboard.jsx";
-import SearchedResults from "./Components/SearchedResults/SearchedResults.jsx";
-import Root from "./Components/Root/Root.jsx";
-import axios from "axios";
-import { FoodContextProvider } from "./userContext/foodContext.jsx";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Cart from './Components/Cart/Cart';
+import SignIn from './Components/Signin/Signin';
+import HomePage from './Components/Homepage/Homepage';
+import RestaurantPage from './Components/RestaurantPage/RestaurantPage';
+import { UserContextProvider } from './userContext/context.jsx';
+import './index.css';
+import Dashboard from './Components/Dashboard/Dashboard.jsx';
+import SearchedResults from './Components/SearchedResults/SearchedResults.jsx';
+import Root from './Components/Root/Root.jsx';
+import axios from 'axios';
+import { FoodContextProvider } from './userContext/foodContext.jsx'; //
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Root />,
-        children: [
-            {
-                index: true,
-                element: <SignIn />,
-            },
-            {
-                path: "homepage",
-                element: <HomePage />,
-            },
-            {
-                path: "homepage/results/:resultName",
-                element: <SearchedResults />,
-            },
-            {
-                path: "homepage/:restaurantId",
-                element: <RestaurantPage />,
-            },
-            {
-                path: "homepage/cart",
-                element: <Cart />,
-            },
-        ],
-    },
-    {
-        path: "restaurantId",
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <SignIn />,
+      },
+      {
+        path: 'homepage',
+        element: <HomePage />,
+      },
+      {
+        path: 'homepage/results/:resultName',
+        element: <SearchedResults />,
+      },
+      {
+        path: 'homepage/:restaurantId',
         element: <RestaurantPage />,
-    },
+      },
+      {
+        path: 'homepage/cart',
+        element: <Cart />,
+      },
+    ],
+  },
+  {
+    path: 'restaurantId',
+    element: <RestaurantPage />,
+  },
 
-    {
-        path: "/dashboard",
-        element: <Dashboard />,
-    },
+  {
+    path: '/dashboard',
+    element: <Dashboard />,
+  },
 ]);
 function App() {
-    axios.defaults.withCredentials = true;
-    return (
-        <div className="wrapper">
-            <FoodContextProvider>
-                <UserContextProvider>
-                    <RouterProvider router={router} />
-                </UserContextProvider>
-            </FoodContextProvider>
-        </div>
-    );
+  axios.defaults.withCredentials = true;
+  return (
+    <div className='wrapper'>
+      <FoodContextProvider>
+        <UserContextProvider>
+          <RouterProvider router={router} />
+        </UserContextProvider>
+      </FoodContextProvider>
+    </div>
+  );
 }
 
 export default App;
