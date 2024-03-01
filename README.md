2ec9fe18b1da29466057a256c29c4dd76b735e98
Kshitij <kshitij.tanwar2002@gmail.com>
2024-03-01 14:30:37 +0530

    tailwind

--- README.md
+++
@@ -1,3 +0,0 @@
-A MERN stack project.
-Aesthetically pleasing design and UI.
-Now order food at ease of a click
--- frontend/src/Components/Utils/AddressBlock/AddressBlock.jsx
+++ frontend/src/Components/Utils/AddressBlock/AddressBlock.jsx
@@ -1,11 +1,10 @@
-import classes from "./AddressBlock.module.css"
-import location from '../../Cart/images/location.svg'
-const AddressBlock = ({address}) => {
-  return (
-    <div className={classes.addrsBlock}>
-        <img src={location} alt="" />
-        <p>{address}</p>
-    </div>
-  )
-}
-export default AddressBlock
+import location from "../../Cart/images/location.svg";
+const AddressBlock = ({ address }) => {
+    return (
+        <div className="border-2 border-dashed border-fudo-red w-fit p-6 rounded-xl lg:w-52 md:w-44 sm:w-40">
+            <img src={location} alt="" />
+            <p>{address}</p>
+        </div>
+    );
+};
+export default AddressBlock;
