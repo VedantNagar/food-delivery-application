215688b831d7eed64c1a77666c00fd728ca2ef85
Kshitij <kshitij.tanwar2002@gmail.com>
2024-03-18 01:35:42 +0530

    create restaurant modal

--- frontend/src/Components/Utils/AdminRestaurantForm/AdminRestaurantForm.jsx
+++ frontend/src/Components/Utils/AdminRestaurantForm/AdminRestaurantForm.jsx
@@ -14,7 +14,7 @@ const AdminRestaurantForm = () => {
                 <div className="relative bg-white rounded-lg shadow ">
                     <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
                         <h3 className="text-lg font-semibold text-gray-900">
-                            Create New Product
+                            Create New Restaurant
                         </h3>
                         <button
                             type="button"
@@ -57,27 +57,72 @@ const AdminRestaurantForm = () => {
                                     name="name"
                                     id="name"
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                                    placeholder="Type product name"
+                                    placeholder="Type restaurant name"
+                                    required=""
+                                />
+                            </div>
+                            <div className="col-span-2">
+                                <label
+                                    htmlFor="name"
+                                    className="block mb-2 text-sm font-medium text-gray-900"
+                                >
+                                    About
+                                </label>
+                                <input
+                                    type="text"
+                                    name="about"
+                                    id="about"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    placeholder="Type about your restaurant"
                                     required=""
                                 />
                             </div>
                             <div className="col-span-2 sm:col-span-1">
                                 <label
-                                    htmlFor="price"
+                                    htmlFor="cft"
                                     className="block mb-2 text-sm font-medium text-gray-900"
                                 >
-                                    Price
+                                    Cost for two
                                 </label>
                                 <input
                                     type="number"
                                     name="price"
                                     id="price"
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                                    placeholder="$2999"
-                                    required=""
+                                    placeholder="$29.99"
                                 />
                             </div>
                             <div className="col-span-2 sm:col-span-1">
+                                <label
+                                    htmlFor="category"
+                                    className="block mb-2 text-sm font-medium text-gray-900 "
+                                >
+                                    Contact number
+                                </label>
+                                <input
+                                    type="number"
+                                    name="contact"
+                                    id="contact"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                    placeholder="+91"
+                                />
+                            </div>
+                            <div className="col-span-2">
+                                <label
+                                    htmlFor="category"
+                                    className="block mb-2 text-sm font-medium text-gray-900 "
+                                >
+                                    Upload restaurant image
+                                </label>
+
+                                <input
+                                    type="file"
+                                    name="file"
+                                    id="contact"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                                />
+                            </div>
+                            {/* <div className="col-span-2 sm:col-span-1">
                                 <label
                                     htmlFor="category"
                                     className="block mb-2 text-sm font-medium text-gray-900 "
@@ -96,13 +141,13 @@ const AdminRestaurantForm = () => {
                                     <option value="GA">Gaming/Console</option>
                                     <option value="PH">Phones</option>
                                 </select>
-                            </div>
+                            </div> */}
                             <div className="col-span-2">
                                 <label
                                     htmlFor="description"
                                     className="block mb-2 text-sm font-medium text-gray-900 "
                                 >
-                                    Product Description
+                                    Restaurant Address
                                 </label>
                                 <textarea
                                     id="description"
@@ -114,7 +159,7 @@ const AdminRestaurantForm = () => {
                         </div>
                         <button
                             type="submit"
-                            className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                            className="text-white inline-flex items-center bg-fudo-red hover:bg-[#e64747] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
                         >
                             <svg
                                 className="me-1 -ms-1 w-5 h-5"
@@ -128,7 +173,7 @@ const AdminRestaurantForm = () => {
                                     clipRule="evenodd"
                                 ></path>
                             </svg>
-                            Add new product
+                            Add new restaurant
                         </button>
                     </form>
                 </div>
