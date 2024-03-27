fbd01706b0083ffdb3387d0ed1d34277a77b929c
Kshitij <kshitij.tanwar2002@gmail.com>
2024-03-27 15:02:24 +0530

    grid fix

--- frontend/src/Components/Homepage/DeliveryRestuarants/DeliveryRestaurants.jsx
+++ frontend/src/Components/Homepage/DeliveryRestuarants/DeliveryRestaurants.jsx
@@ -1,5 +1,3 @@
-import classes from "./DeliveryRestaurants.module.css";
-import styles from "../../Utils/CommonStyles/commonStyles.module.css";
 import RestaurantCard from "./RestaurantCard";
 import { Link, ScrollRestoration } from "react-router-dom";
 import { useContext } from "react";
@@ -8,12 +6,12 @@ import { userContext } from "../../../userContext/context";
 const DeliveryRestaurants = ({ list }) => {
     const { isLoading } = useContext(userContext);
     return (
-        <div className={classes.deliveryRestaurants}>
+        <div className="my-16 flex flex-col flex-wrap justify-center">
             <ScrollRestoration />
-            <h2 className={styles.delivryCollHead}>
+            <h2 className="font-poppins text-xl font-medium">
                 Delivery Restaurants in Delhi NCR
             </h2>
-            <div className={classes.deliveryItems}>
+            <div className="grid xl:grid-cols-3 lg:grid-cols-2 gap-16 mx-auto">
                 {isLoading && <h1>Please wait, loading...</h1>}
                 {list.map((restaurant) => {
                     return (
