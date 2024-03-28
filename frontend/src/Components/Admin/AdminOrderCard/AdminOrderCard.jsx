0921cb610c4f769382172e82acac0587bbbddfc2
Kshitij <kshitij.tanwar2002@gmail.com>
2024-03-28 23:00:56 +0530

    admin create restaurant form

--- frontend/src/Components/Admin/AdminOrderCard/AdminOrderCard.jsx
+++ frontend/src/Components/Admin/AdminOrderCard/AdminOrderCard.jsx
@@ -1,29 +1,29 @@
 import axios from "axios";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { changeStatusUrl } from "../../../../urls/restaurantUrl";
 
-const AdminOrderCard = ({data,id}) => {
-    // console.log(data.user)
-    const [status,setStatus] = useState(data?.orderStatus)
+const AdminOrderCard = ({ data, id }) => {
+    const [status, setStatus] = useState(data?.orderStatus);
     useEffect(() => {
-        const changeStatus = async() => {
-            console.log(status)
-            const response = await axios.patch(`${changeStatusUrl}/${id}`,{
-                orderStatus:status
-            })
-        }
-        changeStatus()
-    },[status])
+        const changeStatus = async () => {
+            const response = await axios.patch(`${changeStatusUrl}/${id}`, {
+                orderStatus: status,
+            });
+        };
+        changeStatus();
+    }, [status]);
     return (
         <div className="border border-slate-300 p-2 rounded-lg flex justify-between gap-4 shadow-md">
             <div>
-                <div className="grid grid-cols-2 ">
-                    <h1 className="text-xl font-medium">{data?.user?.first_name} {data?.user?.last_name}</h1>
+                <div className="grid grid-cols-2 gap-16">
+                    <h1 className="text-xl font-medium">
+                        {data?.user?.first_name} {data?.user?.last_name}
+                    </h1>
                     <h1>{data?.user?.contact}</h1>
                 </div>
                 <span>${data?.totalAmount}</span>
                 <p className="border border-slate-200 rounded-lg p-2">
-                {data?.user?.address}
+                    {data?.user?.address}
                 </p>
             </div>
             <div className="col-span-2 sm:col-span-1">
@@ -36,7 +36,9 @@ const AdminOrderCard = ({data,id}) => {
                 <select
                     id="category"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-                    onChange={(e) => {setStatus(e.target.value)}}
+                    onChange={(e) => {
+                        setStatus(e.target.value);
+                    }}
                 >
                     <option defaultValue="">{status}</option>
                     <option value="preparing">Preparing</option>
