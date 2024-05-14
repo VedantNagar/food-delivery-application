5b61593b6d88d9e89d8de907251306cb060d4708
Vedant <vedantnagar21@gmail.com>
2024-05-14 11:55:17 +0530

    added buttons

--- frontend/src/Components/Admin/AdminOrder/AdminOrder.jsx
+++ frontend/src/Components/Admin/AdminOrder/AdminOrder.jsx
@@ -17,9 +17,18 @@ const AdminOrder = ({id}) => {
         }
         getAllOrder()
     },[])
+    const stauses = ['Preparing', 'Pending', 'Cancelled', 'Delivered'];
     return (
 
         <div className="h-5/6 overflow-y-scroll m-4 rounded-lg flex flex-col gap-3 p-6 shadow-lg">
+            <div>
+                {stauses.map((status,index)=>{
+                    return(<>
+                    <button key={index} className="text-white inline-flex items-center bg-fudo-red hover:bg-[#e64747] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center h-12 mr-5 my-auto">{status}</button>
+                    </>)
+                    
+                })} 
+            </div>
          {data?.length === 0 ? (
                     <h1 className="text-3xl">No Orders</h1>
                 ) : (
