b600c31777e0044b9c4b341a9930de46d4c035d2
araj <araj.jain2003@gmail.com>
2024-06-08 01:36:31 +0530

    cart payment button

--- frontend/src/Components/Admin/AdminOrder/AdminOrder.jsx
+++ frontend/src/Components/Admin/AdminOrder/AdminOrder.jsx
@@ -47,7 +47,7 @@ const AdminOrder = ({ id }) => {
               key={status}
               className={`${
                 status === filterValue
-                  ? "bg-violet-700"
+                  ? "bg-red-600"
                   : "bg-fudo-red hover:bg-[#e64747]"
               }  text-white inline-flex items-center focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center h-12 mr-5 my-auto`}
               onClick={() => checkStatus(status)}
--- frontend/src/Components/Utils/Button/Button.jsx
+++ frontend/src/Components/Utils/Button/Button.jsx
@@ -2,6 +2,13 @@
 const Button = ({onClick,title,type}) => {
   return (
     <button onClick={onClick} className="bg-fudo-red text-white rounded-lg py-4 px-6 cursor-pointer" type={type}>{title}</button>
+    /* 
+    
+      className={`${
+        disabled == false ? "bg-red-400 point cursor-not-allowed" : "bg-fudo-red"
+      } text-white rounded-lg py-4 px-6 cursor-pointer`}
+
+    */
   )
 }
 export default Button
