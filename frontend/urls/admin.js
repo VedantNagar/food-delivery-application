bd58aa14dc10cca0ded5989a225440e14d902b7d
araj <araj.jain2003@gmail.com>
2024-07-05 16:14:44 +0530

    chore: Update API URLs for production environment

--- frontend/urls/admin.js
+++ frontend/urls/admin.js
@@ -1,4 +1,8 @@
-const url = `http://localhost:8000/api/v1/restaurant`
+const url =
+  import.meta.env.PROD === true
+    ? '/api/v1/cart'
+    : 'http://localhost:8000/api/v1/restaurant';
+
 
 export const getAllUserRestUrl = `${url}/getAllUserRestaurant`;
 export const getAllRestOrder = `${url}/getAllOrders`;
--- frontend/urls/payment.js
+++ frontend/urls/payment.js
@@ -1,3 +1,6 @@
-const url = `http://localhost:8000/api/v1/stripe`
+const url =
+  import.meta.env.PROD === true
+    ? '/api/v1/cart'
+    : 'http://localhost:8000/api/v1/stripe';
 
 export const paymentUrl = `${url}/create-checkout-session`;
